ff082ac5305e217bfb93fc9599b29165b82ade57
Yahya M.Mirza <114072734+Yahya123-hub@users.noreply.github.com>
2025-01-24 14:34:30 +0500

    Update tests

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { toContainElement } from '@testing-library/jest-dom/matchers';
 
 test('User can create a room', async ({page}) => {
 
@@ -69,7 +70,7 @@ test('No same usernames in one room', async({page}) =>{
 })
 
 
-test.only('Users can join existing room', async ({page, browser}) =>{
+test('Users can join existing room', async ({browser}) =>{
   const context1 = await browser.newContext();
   const page1 = await context1.newPage();
 
@@ -77,22 +78,98 @@ test.only('Users can join existing room', async ({page, browser}) =>{
   const page2 = await context2.newPage();
   
   await page1.goto("http://localhost:3000/");
-  await page1.locator('input[placeholder="ROOM ID"]').fill("123456")
-  await page1.locator('role=textbox[name="USERNAME"]').fill("Yahya");
-  await page1.locator('text=Join').click();
+  await page2.goto("http://localhost:3000/"); 
 
-  await page2.goto("http://localhost:3000/"); //resets vals n removes yahya
-  await page2.locator('input[placeholder="ROOM ID"]').fill("123456")
-  await page2.locator('role=textbox[name="USERNAME"]').fill("Ahmed");
-  await page2.locator('text=Join').click();
+  await roomcreation(page1, "123456", "Yahya");
+  await roomcreation(page2, "123456", "Ahmed");
+  
+  const clientlistp1= await page1.locator('.clientsList').textContent();
+  console.log(clientlistp1)
 
-  const clientlist= await page.locator('.clientsList').textContent();
-  console.log(clientlist)
+  const clientlistp2= await page2.locator('.clientsList').textContent();
+  console.log(clientlistp2)
 
-  //await expect(clientlist).toContain('Yahya');
-  //await expect(clientlist).toContain('Ahmed');
+  await expect(clientlistp1).toContain('Yahya');
+  await expect(clientlistp1).toContain('Ahmed');
+
+  await expect(clientlistp2).toContain('Ahmed'); 
+  await expect(clientlistp2).toContain('Yahya');
   
 })
-            
+
+async function roomcreation(page, roomid, username) {
+
+  await page.locator('input[placeholder="ROOM ID"]').fill(roomid.toString());
+  await page.locator('role=textbox[name="USERNAME"]').fill(username.toString());
+  await page.locator('text=Join').click();
+
+}
+    
+test("User can copy room id", async ({page}) =>{
+
+  await page.goto("http://localhost:3000/")
+  await roomcreation(page, "12345", "Yahya");
+  await page.locator('text=Copy ROOM ID').click();
+
+  const alertDiv = await page.locator('div[role="status"][aria-live="polite"]').textContent();
+  await expect(alertDiv).toEqual("Room ID has been copied to your clipboard")
+
+})
+
+test("User can leave room", async ({page}) =>{
+
+  await page.goto("http://localhost:3000/")
+  await roomcreation(page, "12345", "Yahya");
+  await page.locator('text=Leave').click();
+  await expect(page.url()).toEqual("http://localhost:3000/");
+
+})
+
+test("Code editor should be empty for a new room", async({page})=>{
+  await page.goto("http://localhost:3000/")
+  await roomcreation(page, "12345", "Yahya");
+  await expect(page.locator('role=presentation')).toHaveText('');
+
+});
+
+/*1Simulate two users editing different parts of the code simultaneously.
+Assert no conficts or race conditions occur
+2Verify that code changes made by one user are visible to all other users in real time.
+3lang selec rt
+4output*/
+
+test("User can select a language", async({page})=>{
+
+  await page.goto("http://localhost:3000/")
+  await roomcreation(page, "1234", "Yahya");
+  const select = await page.locator('select');
+  await select.selectOption("typescript");
+  const val = await select.inputValue();
+  await expect(val).toBe('typescript');
+
+})
+
+test.only("Dropdown languages synchronizes across users in the room", async ({browser}) =>{
+
+  const context1 = await browser.newContext();
+  const page1 = await context1.newPage();
+
+  const context2= await browser.newContext();
+  const page2 = await context2.newPage();
+
+  await page1.goto("http://localhost:3000/")
+  await page2.goto("http://localhost:3000/")
+
+  await roomcreation(page1, "12345", "Yahya");
+  await roomcreation(page2, "12345", "Ahmed"); 
+
+  const selector1= await page1.locator('select');
+  await selector1.selectOption('typescript');
+
+  await page2.waitForTimeout(500); //susssy
+
+  const selector2 = await page2.locator('select');
+  await expect(selector2).toHaveValue('typescript')
 
 
+})
